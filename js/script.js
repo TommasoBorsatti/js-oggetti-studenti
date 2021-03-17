dbe4fb2d9c1b08d8b8328ec60edad14ce670a4e4
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-03-17 15:50:40 +0100

    Script - Logica di business completata.
    L'esercizio è finito ed ho aggiunto un piccolo bonus in cui vado ad aggiungere il nuovo studente iscritto alla classe già esistente.

--- js/script.js
+++ js/script.js
@@ -6,3 +6,74 @@
 // Creare un array di oggetti di studenti.
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+
+//1 - Definisco un oggetto Studente prendendo me stesso come esempio. Inserisco le proprietà nome, cognome e età con relativi chiavi e valori.
+
+var studente1 = {
+
+  nome: "Tommaso",
+  cognome: "Borsatti",
+  eta: 31,
+
+}
+console.log(studente1);
+
+
+//2 - Uso ciclo FOR IN per attraversare l'oggetto; stampo tutte le proprietà, una chiave alla volta.
+
+for (var key in studente1) {
+
+  console.log(key + ": " + studente1[key]);
+
+}
+
+//3 - Creo un Array contentente oggetti/studenti fittizi; ne metto 3.
+
+var studente2 = {
+
+  nome: "Pigiamino",
+  cognome: "Morbidozzi",
+  eta: 28,
+
+}
+
+var studente3 = {
+
+  nome: "Mino",
+  cognome: "Ciccini",
+  eta: 33,
+
+}
+
+var studenti = [studente1, studente2, studente3];
+
+console.log(studenti);
+
+// Uso un ciclo FOR e scorro l'array Studenti; stando attento a scegliere gli indici giusti e le key giuste, stampole le proprietà nome e cognome.
+// Sono costretto a usare la notation con le parentesi quadre.
+// di tutti gli oggetti/studenti contenuti nell'array:
+
+for (var i = 0; i < studenti.length; i++) {
+
+  console.log(studenti[i]["nome"] + " " + studenti[i]["cognome"]);
+
+}
+
+// Uso dei prompt per definire le proprietà che costituiranno un quarto studente/oggetto.
+// Parto da un oggetto vuoto e assegno le proprietà con la dot notation usando = come si fa con l assegnazione di variabili.
+
+var studente4 = {};
+
+studente4.nome = prompt("Definisci il nome del quarto studente della classe");
+studente4.cognome = prompt("Adesso definisci il cognome del quarto studente della classe");
+studente4.eta = prompt("Infine, definisci l'età del quarto studente della classe");
+
+console.log(studente4);
+
+// BONUS - Inserisco il nuovo oggetto studente all'interno dell'array studenti, come se fosse aggiunto alla classe.
+// Ristampo l'array studenti per controllo e verifico se la classe è stata aggiornata con il nuovo studente iscritto.
+
+studenti.push(studente4);
+
+console.log(studenti);
